49e8537cd3aae7e81f990da73683205ae7a47416
Rahat Hossain <rahat1470.com@gmail.com>
2025-11-01 15:47:32 +0600

    fractionally size added

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApplication extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeActivity(),
+      home: FractionalySize(),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -35,6 +35,32 @@ class HomeActivity extends StatelessWidget{
           aspectRatio: 16/9,
           child: Container(
             color: Colors.green,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FractionalySize extends StatelessWidget{
+  const FractionalySize({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rahat Hossain', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          heightFactor: 0.3,
+          child: Container(
+            color: Colors.red,
+            child: Center(
+              child: Text('hello', style: TextStyle(color: Colors.white),),
+            ),
           ),
         ),
       ),
